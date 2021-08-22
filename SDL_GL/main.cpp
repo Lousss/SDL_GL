365e17562e769b67601ea0f49c2caa2572a2a8c3
Sergey Apenkov <sonnteam@gmail.com>
2021-08-22 12:21:31 +0300

    Initial Commit

---
+++ SDL_GL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_GL
+//
+//  Created by Sergey Apenlpv on 22/08/2021.
+//  Copyright © 2021 Sergey Apenkov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
